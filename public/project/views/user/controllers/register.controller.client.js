4a84e7ac6d8110ea03cb8c287fa0760c0adfd9ca
Sushant Mimani <sushant.mimani@gmail.com>
2017-08-13 21:38:40 -0400

    fixed registration

--- public/project/views/user/controllers/register.controller.client.js
+++ public/project/views/user/controllers/register.controller.client.js
@@ -12,20 +12,30 @@
 
         // implementation
         function registerUser(newUser) {
+            if(newUser ==undefined || newUser == null || !newUser.hasOwnProperty('firstName')
+                || !newUser.hasOwnProperty('lastName') || !newUser.hasOwnProperty('username')
+                || !newUser.hasOwnProperty('email') || !newUser.hasOwnProperty('password')
+                || !newUser.hasOwnProperty('password2')){
+                model.error = "All fields are mandatory!"
+                return;
+            } else {
+
+
             userService
                 .findUserByUsername(newUser)
                 .then(login)
 
             function login(found) {
-
-                if(found !== 'available') {
+                console.log(found);
+                if (found !== 'available') {
                     model.error = "Username and/or Email is already registered";
                 } else {
-                    if(newUser.password !== newUser.password2) {
+                    if (newUser.password !== newUser.password2) {
                         model.error = "Passwords must match";
                         return;
                     } else {
-                        delete newUser.password2;
+                        var user = newUser;
+                        delete user.password2;
                         userService
                             .register(user)
                             .then(redirectToProfile)
@@ -36,7 +46,7 @@
                         }
                     }
                 }
-
+            }
 
             }
         }
